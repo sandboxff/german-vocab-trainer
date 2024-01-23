2925b2cf7245fdbfbfe79a4a4f749e261240d6be
sandboxff <153873660+sandboxff@users.noreply.github.com>
2024-01-23 23:51:24 +0100

    Add form validation to WordForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './styles/App.css'
 import WordForm from './components/WordForm'
 
--- src/components/WordForm.tsx
+++ src/components/WordForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import getEnumKeys from "../utils/utils";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -6,6 +6,13 @@ enum PartOfSpeech {
     noun = "noun",
     verb = "verb",
     adjective = "adjective"
+}
+
+enum Level {
+    a1 = "A1",
+    a2 = "A2",
+    b1 = "B1",
+    b2Plus = "B2+"
 }
 
 enum Article {
@@ -18,6 +25,7 @@ interface WordFormInput {
     word: string,
     slovakTranslation: string,
     englishTranslation: string,
+    level: Level,
     partOfSpeech: PartOfSpeech,
     nounDetails?: {
         article: Article,
@@ -40,25 +48,39 @@ interface WordFormInput {
 function WordForm() {
 
     const form = useForm<WordFormInput>();
-    const { register, handleSubmit, unregister } = form
+    const { register, handleSubmit, unregister, formState } = form
     const onSubmit: SubmitHandler<WordFormInput> = (data) => console.log(data);
     const [partOfSpeech, setPartOfSpeech] = useState("noun")
+    const { errors } = formState
     
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="word">German word</label>
-                <input type="text" id="word" {...register("word")}></input>
+                <input type="text" id="word" {...register("word", {required: {value: true, message: "This field is required"}})}></input>
+                <p className="form-validation-error">{errors.word?.message}</p>
 
                 <label htmlFor="slovakTranslation">Slovak translation</label>
-                <input type="text" id="slovakTranslation" {...register("slovakTranslation")}></input>
+                <input type="text" id="slovakTranslation" {...register("slovakTranslation", {required: {value: true, message: "This field is required"}})}></input>
+                <p className="form-validation-error">{errors.slovakTranslation?.message}</p>
 
                 <label htmlFor="englishTranslation">English translation</label>
-                <input type="text" id="englishTranslation" {...register("englishTranslation")}></input>
+                <input type="text" id="englishTranslation" {...register("englishTranslation", {required: {value: true, message: "This field is required"}})}></input>
+                <p className="form-validation-error">{errors.englishTranslation?.message}</p>
+
+                <label htmlFor="level">Level</label>
+                <select id="level" {...register("level", {required: true})}>
+                {getEnumKeys(Level).map((key, index) => (
+                    <option key={index} value={Level[key]}>
+                        {Level[key]}
+                    </option>
+                ))}
+                </select>
+                <p className="form-validation-error">{errors.level?.message}</p>
 
                 <label htmlFor="partOfSpeech">Part of speech</label>
-                <select id="partOfSpeech" {...register("partOfSpeech")} onChange={(e)=>{
+                <select id="partOfSpeech" {...register("partOfSpeech", {required: {value: true, message: "This field is required"}})} onChange={(e)=>{
                     setPartOfSpeech(e.target.value); console.log(e.target.value);
                     if(e.target.value != "noun") {
                         unregister("nounDetails")
@@ -76,49 +98,61 @@ function WordForm() {
                     </option>
                 ))}
                 </select>
+                <p className="form-validation-error">{errors.partOfSpeech?.message}</p>
+
                 {partOfSpeech === "noun" ? (
                     <>
                         <label htmlFor="article">Article</label>
-                        <select id="article" {...register("nounDetails.article")}>
+                        <select id="article" {...register("nounDetails.article", {required: {value: true, message: "This field is required"}})}>
                         {getEnumKeys(Article).map((key, index) => (
                             <option key={index} value={Article[key]}>
                                 {key}
                             </option>
                         ))}
                         </select>
+                        <p className="form-validation-error">{errors.nounDetails?.article?.message}</p>
 
                         <label htmlFor="genitive">Genitive</label>
-                        <input type="text" id="genitive" {...register("nounDetails.genitive")}></input>
+                        <input type="text" id="genitive" {...register("nounDetails.genitive", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.nounDetails?.genitive?.message}</p>
 
                         <label htmlFor="plural">Plural</label>
-                        <input type="text" id="plural" {...register("nounDetails.plural")}></input>
+                        <input type="text" id="plural" {...register("nounDetails.plural", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.nounDetails?.plural?.message}</p>
                     </>
                 ): (<></>) }
                 {partOfSpeech === "verb" ? (
                     <>
                         <label htmlFor="isRegular">Regular</label>
                         <input type="checkbox" id="isRegular" {...register("verbDetails.isRegular", {value: true})}></input>
+                        <p className="form-validation-error">{errors.verbDetails?.isRegular?.message}</p>
 
                         <label htmlFor="present">Present tense (3rd person, singular)</label>
-                        <input type="text" id="present" {...register("verbDetails.present")}></input>
+                        <input type="text" id="present" {...register("verbDetails.present", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.verbDetails?.present?.message}</p>
 
                         <label htmlFor="pastSimple">Past simple (3rd person, singular)</label>
-                        <input type="text" id="pastSimple" {...register("verbDetails.pastSimple")}></input>
+                        <input type="text" id="pastSimple" {...register("verbDetails.pastSimple", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.verbDetails?.pastSimple?.message}</p>
 
                         <label htmlFor="pastPerfect">Past perfect (3rd person, singular)</label>
-                        <input type="text" id="pastPerfect" {...register("verbDetails.pastPerfect")}></input>
+                        <input type="text" id="pastPerfect" {...register("verbDetails.pastPerfect", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.verbDetails?.pastPerfect?.message}</p>
                     </>
                 ): (<></>)}
                 {partOfSpeech === "adjective" ? (
                     <>
                         <label htmlFor="isRegular">Regular</label>
                         <input type="checkbox" id="isRegular" {...register("adjectiveDetails.isRegular", {value: true})}></input>
+                        <p className="form-validation-error">{errors.adjectiveDetails?.isRegular?.message}</p>
 
                         <label htmlFor="comparative">Comparative</label>
-                        <input type="text" id="comparative" {...register("adjectiveDetails.comparative")}></input>
+                        <input type="text" id="comparative" {...register("adjectiveDetails.comparative", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.adjectiveDetails?.comparative?.message}</p>
 
                         <label htmlFor="superlative">Superlative</label>
-                        <input type="text" id="superlative" {...register("adjectiveDetails.superlative")}></input>
+                        <input type="text" id="superlative" {...register("adjectiveDetails.superlative", {required: {value: true, message: "This field is required"}})}></input>
+                        <p className="form-validation-error">{errors.adjectiveDetails?.superlative?.message}</p>
                     </>
                 ): (<></>)}
                 <input type="submit" />
